7508ab78428e95457dd3206e00f36a78cccae084
be9904 <be9904@gmail.com>
2022-11-02 00:03:04 +0900

    add projection lines

--- Assets/Project/Scripts/ProjectorControl.cs
+++ Assets/Project/Scripts/ProjectorControl.cs
@@ -5,17 +5,24 @@ using UnityEngine.VFX;
 
 public class ProjectorControl : MonoBehaviour
 {
+    private const string VFXPositionPostfix = "_position";
+    private const string VFXRotationPostfix = "_angles";
+    private const string VFXScalePostfix = "_scale";
+
     public VisualEffect vfx;
     public Transform projectionPoint;
+    private string vfxPositionVar;
     
     // Start is called before the first frame update
     void Start()
     {
         vfx = GetComponent<VisualEffect>();
+        // var vfxPositionVar = "ProjectorPosition" + VFXPositionPostfix;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // vfx.SetVector3(vfxPositionVar, projectionPoint.position);
     }
 }
